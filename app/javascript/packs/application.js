94b8093074dc7da8575c23ceb3dc2d782d0a1284
PapaKramb <guccigang5554@gmail.com>
2022-02-16 00:06:00 +0300

    Add nav and index template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap" 
 import "../stylesheets/application"
+import "../styles/style"
 
 Rails.start()
 Turbolinks.start()
