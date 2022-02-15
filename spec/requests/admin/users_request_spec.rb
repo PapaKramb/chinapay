64dad56f858892a0d4812ae36b6cb7d9c4f26750
PapaKramb <guccigang5554@gmail.com>
2022-02-15 21:56:33 +0300

    Fix users_request_spec

--- spec/requests/admin/users_request_spec.rb
+++ spec/requests/admin/users_request_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe "Admin::Users", type: :request do
     user = create(:user) 
     expect { delete "/admin/users/#{user.id}" }.to change(User, :count).by -1 
   end
+
+  # it "#index" do
+  #   user_1 = create(:user)
+  #   user_2 = create(:user)
+  #   get '/admin/users'
+  #   expect(assigns(:users)).to contain_exactly *[users]
+  # end
+
+  # it "new" do
+  #   get "/admin/users/new"
+  #   expect(assigns(:user)).to be_a_new User
+  # end
+
+  # it "create" do
+  #   expect {
+  #     post "/admin/users/",
+  #       params: {
+  #         user: attributes_for(:user)
+  #       }
+  #   }.to change(User, :count).by 1
+  # end
+
+  # it "delete" do
+  #   user = create(:user)
+  #   expect {
+  #     delete "/admin/users/#{user.id}"
+  #   }.to change(User, :count).by(-1)
+  #   expect(response).to be_redirect
+  # end
 end
